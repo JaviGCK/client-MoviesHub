073b3a8f5512bbacf20a6eec26d32f88e446a5de
Javier García <javigcpgr@gmail.com>
2023-11-06 17:22:42 +0100

    feat(url vercel)

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -1,6 +1,6 @@
 import { handleErrorResponse } from "../utils";
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://server-movies-oo55-on2w86kpo-javigcks-projects.vercel.app/';
 
 
 export const fetchDataAllUsers = async (accessToken: string): Promise<UserData[]> => {
